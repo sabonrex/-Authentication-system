dadd4deb25453f94abeb9833e5eee12719ff1c50
sabonrex <118281670+sabonrex@users.noreply.github.com>
2023-05-06 21:16:09 +0000

    update

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,74 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+	const { store, actions } = useContext(Context);
+	const { setStore } = actions; 
+
+	const handleDelete = (index) => {
+		let store = getStore();
+		let arrTemp = store.favoritos.slice(); 
+		arrTemp.splice(index, 1);
+		setStore({ ...store, favoritos: arrTemp });
+	};
+
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
+		<nav className="navbar navbar-dark bg-dark">
+			<div className="container-fluid">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<img className="img-responsive h-25 w-25" src="https://lumiere-a.akamaihd.net/v1/images/sw_logo_stacked_2x-52b4f6d33087_7ef430af.png?region=0,0,586,254" />
 				</Link>
+				<Link to="/login">
+					<button className="btn btn-warning rounded-pill me-5" type="button">Login</button>
+				</Link>
+				{store.userLogin ?
+					<Link to="/info">
+						<button className="btn btn-warning rounded-pill me-5" type="button">Info</button>
+					</Link>
+					: <></>}
+				<Link to="/register">
+					<button className="btn btn-warning rounded-pill me-5" type="button">Register</button>
+				</Link>
+				{store.userLogin ?
+					<Link to="/info">
+						<button className="btn btn-warning rounded-pill me-5" type="button">Info</button>
+					</Link>
+					: <></>}
+				<div>
+					<div className="nav-item dropdown me-5">
+						<div className="dropdown">
+							<button className="btn btn-warning dropdown-toggle rounded-pill me-5" type="button" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+								Favorites
+							</button>
+							<ul className="dropdown-menu list-unstyled" style={{ width: '200px' }} aria-labelledby="navbarDropdown">
+								{store.favoritos && store.favoritos.length > 0 ? (
+									<>
+										{store.favoritos.map((item, index) => {
+											return (
+												<div key={index} className="d-flex justify-content-between align-items-center" style={{ paddingLeft: '1rem', paddingRight: '1rem' }}>
+													<Link to={item.link} style={{ color: 'black', textDecoration: 'none', fontSize: '15px' }}>
+														{item.name}
+													</Link>
+													<i className="fas fa-trash-alt text-danger h6 p-1" onClick={() => actions.handleDelete(index)} style={{ cursor: 'pointer' }}></i>
+												</div>
+											);
+										})}
+									</>
+								) : (
+									<></>
+								)}
+							</ul>
+						</div>
+					</div>
+				</div>
 				<div className="ml-auto">
 					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+						<button className="btn btn-sm btn-secondary rounded-pill me-5" type="button">Check the Context in action</button>
 					</Link>
 				</div>
 			</div>
-		</nav>
+		</nav >
 	);
 };
+
--- src/front/js/pages/signup.jsx
+++ src/front/js/pages/signup.jsx
@@ -1,1 +1,82 @@
+import React, { useState, useContext } from "react";
+import { Context } from "../store/appContext";
 
+const Register = () => {
+    const { store, actions } = useContext(Context);
+    const [name, setName] = useState("");
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async () => {
+        try {
+            const response = await actions.login(email, password);
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <>
+            <div className="container-fluid">
+                <div className="contactForm container border mt-5 pb-5">
+                    <div className="d-flex justify-content-center">
+                        <h1 className="fs-1 fw-bold mt-5">Register</h1>
+                    </div>
+                    <div className="form-control border border-0 ps-4 pe-4">
+                        <form>
+                            <label htmlFor="name" className="form-label fs-5">
+                                Name
+                            </label>
+                            <input
+                                className="form-control mb-3"
+                                type="text"
+                                id="name"
+                                placeholder="Enter name"
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                            <label htmlFor="name" className="form-label fs-5">
+                                Username
+                            </label>
+                            <input
+                                className="form-control mb-3"
+                                type="text"
+                                id="username"
+                                placeholder="Enter username"
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                            <label htmlFor="email" className="form-label fs-5">
+                                Email
+                            </label>
+                            <input
+                                className="form-control mb-3"
+                                type="email"
+                                id="email"
+                                placeholder="Enter email"
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <label htmlFor="password" className="form-label fs-5">Password</label>
+                            <input
+                                type="password"
+                                className="form-control mb-3"
+                                id="password"
+                                placeholder="Enter password"
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <div className="d-flex justify-content-center">
+                                <button
+                                    type="button"
+                                    className="button-save col-md-2 btn btn-success fs-6 mt-3"
+                                    onClick={handleLogin}>
+                                    Register
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>)
+}
+
+export default Register;
